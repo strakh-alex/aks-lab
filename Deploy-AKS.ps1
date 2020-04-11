2253ed21ffe0992f1ffe4a6f8321779cc56ea41d
Strakh Alex <strakh.alex@gmail.com>
2020-04-11 19:00:34 +0300

    Add KeyVault

--- Deploy-AKS.ps1
+++ Deploy-AKS.ps1
@@ -1,9 +1,3 @@
-# 
-# AKS VMSS Cluster template: https://github.com/Azure/azure-quickstart-templates/tree/master/kubernetes-on-ubuntu-vmss
-#
-# The example of Kubernetes application: https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/
-#
-
 param(
     [string]$TemplateFilePath = ".\template.json",
     [string]$TemplateFileParametersPath = ".\parameters.json",
@@ -27,11 +21,12 @@ $Parameters.parameters.linuxAdminUsername.value = $AKSadminUsername
 $Parameters.parameters.sshRSAPublicKey.value = $AKSadminKey.ToString()
 $Parameters.parameters.servicePrincipalClientId.value = $SPparameters.appId
 $Parameters.parameters.servicePrincipalClientSecret.value = $SPparameters.password
+$Parameters.parameters.objectId.value = $SPparameters.objectId
 $Parameters | ConvertTo-Json | Set-Content $TemplateFileParametersPath -Encoding UTF8 -Force
 
 New-AzResourceGroup -Name $ResouceGroup -Location $Location
-New-AzResourceGroupDeployment -ResourceGroupName $ResouceGroup -TemplateFile $TemplateFilePath -TemplateParameterFile $TemplateFileParametersPath
+New-AzResourceGroupDeployment -ResourceGroupName $ResouceGroup -TemplateFile $TemplateFilePath -TemplateParameterFile $TemplateFileParametersPath -Mode Incremental
 
 #az aks install-cli
-az aks get-credentials --resource-group $ResouceGroup --name "aks101cluster"
+#az aks get-credentials --resource-group $ResouceGroup --name "aks101cluster"
 #kubectl get nodes
